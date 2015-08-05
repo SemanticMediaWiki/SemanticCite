ae93c0559dc43ea8a25ac216175e01da8674b80b
Karsten Hoffmeyer <mediawiki@kghoffmeyer.de>
2015-08-05 22:24:06 +0200

    Update SemanticCite.magic.php
    Swap behaviour switches

--- i18n/SemanticCite.magic.php
+++ i18n/SemanticCite.magic.php
@@ -11,7 +11,7 @@ $magicWords = array();
 $magicWords['en'] = array(
 	'scite' => array( 0, 'scite' ),
 	'referencelist' => array( 0, 'referencelist' ),
-	'SCI_NOREFERENCELIST' => array( 0, '__NOREFERENCELIST__' ),
+	'SCI_NOREFERENCELIST' => array( 0, '__NOREFERENCELIST__' )
 );
 
 /**
@@ -19,5 +19,5 @@ $magicWords['en'] = array(
  */
 $magicWords['de'] = array(
 	'referencelist' => array( 0, 'referenzierungen' ),
-	'SCI_NOREFERENCELIST' => array( 0, '__KEINEREFERENZIERUNGEN__', '__KEINE_REFERENZIERUNGEN__' ),
+	'SCI_NOREFERENCELIST' => array( 0, '__KEINE_REFERENZIERUNGEN__', '__KEINEREFERENZIERUNGEN__' )
 );
