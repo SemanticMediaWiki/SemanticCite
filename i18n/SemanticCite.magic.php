d06a8c26a604fd1dc8c777806694475003a438ec
Karsten Hoffmeyer <mediawiki@kghoffmeyer.de>
2015-08-05 22:18:33 +0200

    Update SemanticCite.magic.php
    Add German translations

--- i18n/SemanticCite.magic.php
+++ i18n/SemanticCite.magic.php
@@ -13,3 +13,11 @@ $magicWords['en'] = array(
 	'referencelist' => array( 0, 'referencelist' ),
 	'SCI_NOREFERENCELIST' => array( 0, '__NOREFERENCELIST__' ),
 );
+
+/**
+ * German (Deutsch)
+ */
+$magicWords['de'] = array(
+	'referencelist' => array( 0, 'referenzierungen' ),
+	'SCI_NOREFERENCELIST' => array( 0, '__KEINEREFERENZIERUNGEN__', '__KEINE_REFERENZIERUNGEN__' ),
+);
