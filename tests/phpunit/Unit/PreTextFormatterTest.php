9ed58228d6938a018995dbff2c60d98476581f45
mwjames <jamesin.hongkong.1@gmail.com>
2017-05-28 13:06:44 +0900

    Add test

---
+++ tests/phpunit/Unit/PreTextFormatterTest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace SCI\Tests;
+
+use SCI\PreTextFormatter;
+use Title;
+use Parser;
+use ParserOptions;
+
+/**
+ * @covers \SCI\PreTextFormatter
+ * @group semantic-cite
+ *
+ * @license GNU GPL v2+
+ * @since 1.4
+ *
+ * @author mwjames
+ */
+class PreTextFormatterTest extends \PHPUnit_Framework_TestCase {
+
+	const LF = "\n";
+
+	private $parser;
+
+	protected function setUp() {
+		parent::setUp();
+
+		$this->parser = new Parser();
+		$this->parser->Options( new ParserOptions() );
+		$this->parser->clearState();
+	}
+
+	public function testCanConstruct() {
+
+		$this->assertInstanceOf(
+			'\SCI\PreTextFormatter',
+			new PreTextFormatter()
+		);
+	}
+
+	/**
+	 * @dataProvider parametersProvider
+	 */
+	public function testFormat( $params ) {
+
+		$instance = new PreTextFormatter();
+
+		$this->assertInternalType(
+			'array',
+			$instance->format( $params )
+		);
+	}
+
+	/**
+	 * @dataProvider parametersProvider
+	 */
+	public function testGetFormattedSciteFuncFrom( $params, $expected ) {
+
+		$instance = new PreTextFormatter();
+
+		$this->assertSame(
+			$expected,
+			$instance->getFormattedSciteFuncFrom( $params )
+		);
+	}
+
+	public function parametersProvider() {
+
+		$provider[] = array(
+			array(
+				'Bar',
+				'@Foobar'
+			),
+			"<pre>{{#scite:" . self::LF . " |Bar" . self::LF . "}}</pre>"
+		);
+
+		$provider[] = array(
+			array(
+				'Bar',
+				'+sep=,',
+				'@Foobar',
+				'Foo'
+			),
+			"<pre>{{#scite:" . self::LF . " |Bar|+sep=," . self::LF . " |Foo" . self::LF . "}}</pre>"
+		);
+
+		return $provider;
+	}
+
+}
